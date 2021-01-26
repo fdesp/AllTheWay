a3af847250f5853ab40fec3678645879def293ab
A. G. Medrano-Chávez <ryuuba@gmail.com>
2021-01-26 00:01:13 -0600

    Fix bug in locaFlooding

--- src/BaseNode.cc
+++ src/BaseNode.cc
@@ -17,7 +17,7 @@ omnetpp::cMessage* BaseNode::localFlooding(
 ) {
   if (msg) {
     int n = gateSize("port$o");
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n; i++)
       if (i != senderID)
         send(msg->dup(), "port$o", i);
     delete msg;
