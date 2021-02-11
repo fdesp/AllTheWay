272df9e17b5392d26e616d203f692e3d8f0c0593
A. G. Medrano-Chávez <ryuuba@gmail.com>
2021-02-11 14:00:22 -0600

    Update Status.h

--- src/Status.cc
+++ src/Status.cc
@@ -6,3 +6,7 @@ const Status Status::IDLE(1);
 const Status Status::ACTIVE(2);
 const Status Status::SLEEP(3);
 const Status Status::DONE(4);
+const Status Status::LEADER(5);
+const Status Status::FOLLOWER(6);
+const Status Status::SATURATED(7);
+const Status Status::PROCESSING(8);
